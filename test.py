bfd57e3efc8c44b1c8dd097d5b614d44c0e438a2
jchen8tw <11806483+jchen8tw@users.noreply.github.com>
2018-12-28 16:13:36 +0800

    pass 460000 data

--- test.py
+++ test.py
@@ -1,13 +1,13 @@
 import numpy as np
 f = open('testdo','w+')
-f.write('taskinit 46000\n')
+f.write('taskinit 460000\n')
 comd = []
 for i in range(26):
     for j in range(26):
         for k in range(26):
-            #for l in range(26):
-            #comd.append('tasknew -n '+chr(ord('a')+i)+chr(ord('a')+j)+chr(ord('a')+k)+chr(ord('a')+l)+' '+str(l+k*26+j*26*26+i*26*26*26) +'\n')
-            comd.append('tasknew -n '+chr(ord('a')+i)+chr(ord('a')+j)+chr(ord('a')+k)+' '+str(k+j*26+i*26*26) +'\n')
+            for l in range(26):
+                comd.append('tasknew -n '+chr(ord('a')+i)+chr(ord('a')+j)+chr(ord('a')+k)+chr(ord('a')+l)+' '+str(l+k*26+j*26*26+i*26*26*26) +'\n')
+                #comd.append('tasknew -n '+chr(ord('a')+i)+chr(ord('a')+j)+chr(ord('a')+k)+' '+str(k+j*26+i*26*26) +'\n')
 comda =  np.array(comd)
 del comd
 np.random.shuffle(comda)
@@ -15,6 +15,10 @@ for i in range(len(comda)):
     f.write(comda[i])
 f.write('taskremove -r 1000\n')
 f.write('taskq -he\n')
+f.write('taskq -ha\n')
+f.write('taska 460000 -r 1000\n')
+f.write('taskq -he\n')
+f.write('taskq -ha\n')
 f.write('usage\n')
 f.write('q -f\n')
 f.close()
